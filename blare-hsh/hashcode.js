59ae4206739033dba413a00352136760ec2ab403
ZareZare3 <39651113+ZareZare3@users.noreply.github.com>
2020-01-21 10:09:33 -0800

    Added the encryption system

--- blare-hsh/hashcode.js
+++ blare-hsh/hashcode.js
@@ -8,19 +8,27 @@ class BlareHash{
  var data;
  var identityNms;
  var indentityChar;
- function BlareHash(uname, upass){
+ function BlareHash(str){
  if(isRandom){
  data = randInt(randInt(100000000000));
  
  }else{
+  
   for(var i = 0;i <= uname.length;i++){
-  
-  
+   data += str.charCodeAt(i);
+   
   
   }
   
   
  }
+  
  }
-
+ 
+function addEncryptData(){
+identityChar.fromCharCode(randInt(10));
+identityNms = randInt(50);
+ 
+} 
+ 
 }
