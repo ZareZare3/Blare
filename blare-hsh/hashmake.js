9e68a764f25009bac9ae3521900a664088b08d2a
ZareZare3 <39651113+ZareZare3@users.noreply.github.com>
2020-01-20 17:52:58 -0800

    adding Login Info System for Blare

---
+++ blare-hsh/hashmake.js
@@ -0,0 +1,26 @@
+
+function randInt(max){
+//always starts at 0
+return Math.floor(Math.random() * (max*2));
+
+}
+class BlareHash{
+ var data;
+ var identityNms;
+ var indentityChar;
+ function BlareHash(uname, upass){
+ if(isRandom){
+ data = randInt(randInt(100000000000));
+ 
+ }else{
+  for(var i = 0;i <= uname.length;i++){
+  
+  
+  
+  }
+  
+  
+ }
+ }
+
+}
