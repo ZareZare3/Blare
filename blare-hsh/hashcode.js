c9ec5f3142b4f94441bc70568f7b21eb9eab54ea
ZareZare3 <39651113+ZareZare3@users.noreply.github.com>
2020-01-22 10:25:31 -0800

    incomplete commit

--- blare-hsh/hashcode.js
+++ blare-hsh/hashcode.js
@@ -41,7 +41,7 @@ splitHash.from(hash.toString());
  var individualNums = new Array();
  var currentCharNo = 0;
  var idStart = false;
- var identityHash;
+ var identityHash = "";
  //time to parse the array
  //we can just use hash's length for the for loop because they have the same length
  for(var i = 0;i < hash.toString().length;i++){
@@ -49,7 +49,7 @@ splitHash.from(hash.toString());
   if(!idStart){
  individualNums[currentCharNo] += splithash[i];
   }else{
-  
+  identityHash.push(splithash[i]);
   
   
   }
@@ -64,6 +64,11 @@ splitHash.from(hash.toString());
  
  }
  //now its time to extract the numbers and feed them into the ASCII code-to-char converter.
- 
+ var identityInt = identityHash.parseInt();
+  var finalNo;
+  for (var i = 0;i < individualNums.length; i++){
+  finalNo += ind
+  
+  }
  }
 }
